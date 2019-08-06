923ec55f4266463ca813f6b175d39b1fe7e937e5
dayvar14 <dayvar141@gmail.com>
2019-08-06 14:35:38 -0500

    removed canvas module

--- client/client.js
+++ client/client.js
@@ -1,9 +1,5 @@
 const mongoose = require('mongoose');
 const Heatmap = require('../schemas/heatmap');
-const { createCanvas, loadImage } = require('canvas')
-const canvas = createCanvas(1000, 1000);
-
-var heat = simpleheat(canvas);
 
 mongoose.connect('mongodb://localhost/NEIUHeatMap', { useNewUrlParser: true })
     .then(() => console.log('Connected to MongoDB...'))
@@ -11,7 +7,7 @@ mongoose.connect('mongodb://localhost/NEIUHeatMap', { useNewUrlParser: true })
 
 async function findAllDocuments() {
     let firstDate = new Date();
-    const result = await Heatmap.find({}).select("-_id -lastUpdate -__v").sort({ x: 1, y: 1 }).lean();
+    const result = await Heatmap.find({}).select("x y count").sort({ count:-1 }).lean();
 
     array = []
     for (key in result) {
@@ -21,8 +17,6 @@ async function findAllDocuments() {
     let secondDate = new Date();
     let timeDifference = Math.ceil((secondDate.getTime() - firstDate.getTime()));
     console.log("Updated Heatmap in " + timeDifference + "s");
-
-    
 }
 
 result = findAllDocuments();
